78094afaeddd6eb233c6efdfafad3fae8fe2ffd1
Adil <99889589+Ad1l9@users.noreply.github.com>
2023-10-23 00:20:27 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedList<int, string> ProgrammingLanguanges = new SortedList<int, string>
+            {
+                { 1980, "C++" },
+                { 1995, "Java" },
+                { 1990, "Python" },
+                { 2001, "C#" },
+                { 2004, "Adil" },
+            };
+            foreach (var language in ProgrammingLanguanges)
+            {
+                Console.WriteLine(language);
+            }
+        }
+    }
+}
